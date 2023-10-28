5cdcc03a06f64ab4d74347e8fcd9714e7b72e03f
ahmaddredi-cell <ahmad95dredipro@gmail.com>
2023-10-29 00:03:48 +0300

    CRUD Category,creat,update,getall,getSpec

--- src/modules/categories/categories.controller.js
+++ src/modules/categories/categories.controller.js
@@ -2,9 +2,16 @@ import slugify from "slugify";
 import cloudinary from "../../services/cloudinary.js";
 import categoryModel from "../../../DB/model/category.model.js";
 
-export const getCategories = (req, res) => {
-  return res.json({ message: "Categories" });
+export const getCategories = async (req, res) => {
+  const categoryList = await categoryModel.find();
+  return res.status(200).json({ message: "success", categoryList });
 };
+export const getSpecficCategories = async (req, res) => {
+  const { id } = req.params;
+  const category = await categoryModel.findById(id);
+  return res.status(200).json({ message: "success", category });
+};
+
 export const createCategory = async (req, res) => {
   const name = req.body.name.toLowerCase();
 
@@ -17,4 +24,38 @@ export const createCategory = async (req, res) => {
   });
   const cat = await categoryModel.create({ name, slug: slugify(name), image: { secure_url, public_id } });
   return res.status(201).json({ message: "success", cat });
+};
+export const updateCategory = async (req, res) => {
+  try {
+    const category = await categoryModel.findById(req.params.id);
+    if (!category) {
+      return res.status(404).json({ message: `Invaliad category id ${req.params.id}` });
+    }
+    if (req.body.name) {
+      if (await categoryModel.findOne({ name: req.body.params }).select("name")) {
+        return res.status(409).json({ message: `catecogy ${req.body.name} already exists` });
+      }
+
+      category.name = req.body.name;
+      category.slug = slugify(req.body.name);
+    }
+    if (req.body.status) {
+      category.status = req.body.status;
+    }
+    if (req.file) {
+      const { secure_url, public_id } = await cloudinary.uploader.upload(req.file.path, {
+        folder: `${process.env.APP_NAME}/categories`,
+      });
+      await cloudinary.uploader.destroy(category.image.public_id);
+      category.image = { secure_url, public_id };
+    }
+    await category.save();
+    return res.status(200).json({ message: "success" });
+  } catch (err) {
+    return res.status(500).json({ message: "error", err: err });
+  }
+};
+export const activeCategories = async (req, res) => {
+  const categoryActive = await categoryModel.find({ status: "Active" }).select("name image");
+  return res.status(200).json({ message: "success", categoryActive });
 };
--- src/modules/categories/categories.router.js
+++ src/modules/categories/categories.router.js
@@ -3,5 +3,9 @@ import * as categriesController from "./categories.controller.js";
 import fileUpload, { fileValidation } from "../../services/multer.js";
 const router = Router();
 router.get("/", categriesController.getCategories);
+router.get("/active", categriesController.activeCategories);
+router.get("/:id", categriesController.getSpecficCategories);
+
 router.post("/", fileUpload(fileValidation.image).single("image"), categriesController.createCategory);
+router.put("/:id", fileUpload(fileValidation.image).single("image"), categriesController.updateCategory);
 export default router;
